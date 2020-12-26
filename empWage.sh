2e4edf81a4da91a81dc7a1323432041353317a3f
joel3216 <joeljaxon3216@gmail.com>
2020-12-26 21:04:47 +0530

    ADD. added a limit of 100 working hours

--- empWage.sh
+++ empWage.sh
@@ -8,24 +8,32 @@ halfDay=4
 empHrs=0
 daysInMonth=20
 monthlyWage=0
+workedHrs=0
+maxHrs=100
 
 for (( i=0; i< $daysInMonth; i++ ))
 do
-	isPresent=$((RANDOM%2))
-	workHrCheck=$((RANDOM%2))
 
-	if [[ $isPresent -eq 1 ]]
+	if [[ $workedHrs -le $maxHrs ]]
 	then
-		case $workHrCheck in
-			1)
-				empHrs=$fullDay
-				;;
-			0)
-				empHrs=$halfDay
-				;;
-		esac
-	fi
+		isPresent=$((RANDOM%2))
+		workHrCheck=$((RANDOM%2))
 
+		if [[ $isPresent -eq 1 ]]
+		then
+			case $workHrCheck in
+				1)
+					empHrs=$fullDay
+					;;
+				0)
+					empHrs=$halfDay
+					;;
+			esac
+			workedHrs=$(($workedHrs+$empHrs))
+		fi
+	else
+		break
+	fi
 	salary=$(($empHrs*$wagePerHr*$isPresent))
 	monthlyWage=$(($monthlyWage+$salary))
 done
