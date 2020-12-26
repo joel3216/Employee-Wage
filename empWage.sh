424cc705022df6640c6bed5852928a7350ae1363
joel3216 <joeljaxon3216@gmail.com>
2020-12-26 20:25:48 +0530

    ADD. added code to check if employee is part-time or full-time worker

--- empWage.sh
+++ empWage.sh
@@ -3,7 +3,17 @@
 echo "Welcome to Employee Wage Computation"
 
 empCheck=$((RANDOM%2))
+workHrCheck=$((RANDOM%2))
 wagePerHr=20
 fullDay=8
+halfDay=4
+empHrs=0
 
-salary=$(($fullDay*$wagePerHr*$empCheck))
+if [[ $workHrCheck -eq 1 ]]
+then
+	empHrs=$fullDay
+else
+	empHrs=$halfDay
+fi
+
+salary=$(($empHrs*$wagePerHr*$empCheck))
